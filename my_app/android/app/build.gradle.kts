25dab946f4c9bd0e37f772443565d6a9e9a4ada8
Pandapapamama <newbiebn58@gmail.com>
2025-04-23 19:06:03 +0700

    edit build.gradle.kts

--- my_app/android/app/build.gradle.kts
+++ my_app/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.my_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,11 +21,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.my_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23  // Explicitly set to 21 or higher (23 recommended for Firebase)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
